d8e86e77079f12975b8265c3e96c5760219c4f73
seojeon9 <underloose18@gmail.com>
2025-04-01 23:37:59 +0900

    Create test2_dag.py

---
+++ test2_dag.py
@@ -0,0 +1,28 @@
+# 필요 모듈 import
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# DAG 정의
+dag = DAG(
+    'DAG_test',         # DAG_Name
+    default_args={                  
+        'retries': 1,               # 실패시 재시도 횟수
+        },
+    schedule_interval='@once',      # 배치 주기
+    start_date=datetime(2023,4, 3)  # 배치 시작날짜 설정
+)
+
+# 함수 설정
+def print_test():
+    print('Hello World')
+
+# task 설정
+t1 = PythonOperator( 
+    task_id = 'print_test', #task 이름 설정
+    python_callable=print_test, # 불러올 함수 설정
+    dag=dag #dag 정보 
+)
+
+# task 진행
+t1
