2efe73152bfc00f9aa6108e93bc8f95e60a2f8ee
seojeon9 <underloose18@gmail.com>
2025-04-02 14:14:16 +0900

    Update test_k8s.py
    - add custom image

--- dags/test_k8s.py
+++ dags/test_k8s.py
@@ -24,10 +24,23 @@ test_task = KubernetesPodOperator(
     cmds=["python", "-c"],
     arguments=["print('Hello from Kubernetes Executor!')"],
     labels={"test": "k8s_executor"},
-    name="python_test",
-    task_id="python_test",
+    name="python_image",
+    task_id="python_image",
     get_logs=True,
     dag=dag,
 )
 
-test_task
+test_task2 = KubernetesPodOperator(
+    namespace="airflow",  # Airflow가 실행되는 네임스페이스
+    pod_template_file="/root/airflow/dags/template/pod_template.yaml",
+    image = "172.31.11.141:5000/comp-image:v1.0.0",
+    cmds = ["python", "-u", f"/app/crawler_main.py", "hansome"],
+    arguments=["print('Hello from Kubernetes Executor!')"],
+    labels={"test": "k8s_executor"},
+    name="cumstom_comp_image",
+    task_id="cumstom_comp_image",
+    get_logs=True,
+    dag=dag,
+)
+
+test_task2
